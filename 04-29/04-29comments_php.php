91cb44b78c3f0f64bd605d1803a5371d039b7ffd
SSU-YUAN <biosvlsi0103@gmail.com>
2026-04-29 15:44:33 +0800

    新增0429上課實作

---
+++ 04-29/04-29comments_php.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<!-- php單行註解 -->
+
+
+<?php 
+// php多行註解
+// echo "hello world";
+// echo "hello world";
+// echo "hello world";
+// echo "hello world";
+// echo "hello world";
+
+
+/*echo "hello world";
+echo "hello world";
+echo "hello world";
+echo "hello world";*/
+
+?>
+    
+</body>
+</html>
